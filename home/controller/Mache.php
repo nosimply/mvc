d5ef60e87d0c7e9f26ad74cde3b7e9b707ab70f8
数木瓜 <38586066+nosimply@users.noreply.github.com>
2021-09-08 15:41:07 +0800

    Add files via upload
    lot项目后端，有些已经丢失。

---
+++ home/controller/Mache.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace app\home\controller;
+
+use think\Controller;
+use think\Request;
+
+class Mache extends Controller
+{
+    /**
+     * 显示资源列表
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+        $user_name=session('user_id');
+        $list=\app\home\model\Mache::where('belong_user',$user_name)->paginate();
+        return view('mache/mache',['list'=>$list,'user'=>$user_name]);
+    }
+
+    /**
+     * 显示创建资源表单页.
+     *
+     * @return \think\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * 保存新建的资源
+     *
+     * @param  \think\Request  $request
+     * @return \think\Response
+     */
+    public function save(Request $request)
+    {
+        //
+    }
+
+    /**
+     * 显示指定的资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function read($id)
+    {
+        //
+    }
+
+    /**
+     * 显示编辑资源表单页.
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * 保存更新的资源
+     *
+     * @param  \think\Request  $request
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * 删除指定资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+}
